2e339e6cd5666554b9a78ba8d46aa45395edbba3
yangzy <306525121@qq.com>
2024-03-07 15:45:21 +0800

    新增排课约束

--- teacher-course/src/main/java/cn/codeyang/course/opta/constraint/CoursePlanConstraintProvider.java
+++ teacher-course/src/main/java/cn/codeyang/course/opta/constraint/CoursePlanConstraintProvider.java
@@ -7,6 +7,8 @@ import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.Joiners;
 
+import java.time.Duration;
+
 /**
  * @author yangzy
  */
@@ -15,7 +17,9 @@ public class CoursePlanConstraintProvider implements ConstraintProvider {
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
         return new Constraint[] {
                 teacherConflict(constraintFactory),
-                classInfoConflict(constraintFactory)
+                classInfoConflict(constraintFactory),
+                courseTypeConflict(constraintFactory),
+                teacherTimeEfficiency(constraintFactory)
         };
     }
 
@@ -54,4 +58,39 @@ public class CoursePlanConstraintProvider implements ConstraintProvider {
                 .penalize(HardSoftScore.ONE_HARD)
                 .asConstraint("classInfo conflict");
     }
+
+    /**
+     * 课程类型约束，正常课时和白天自习课不能安排到早晚自习
+     * @param constraintFactory
+     * @return
+     */
+    Constraint courseTypeConflict(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                // Select each pair of 2 different lessons ...
+                .forEach(CoursePlanOpta.class)
+                .filter((coursePlan) -> coursePlan.getCourseTypeId() == 1 && (coursePlan.getTimeSlot().getType()==1 || coursePlan.getTimeSlot().getType()==2))
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("courseType conflict");
+    }
+
+
+    /**
+     * 教师喜欢按顺序授课，不喜欢课程之间有空隙
+     * @param constraintFactory
+     * @return
+     */
+    Constraint teacherTimeEfficiency(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(CoursePlanOpta.class)
+                .join(CoursePlanOpta.class, Joiners.equal(CoursePlanOpta::getTeacherId),
+                        Joiners.equal((coursePlan) -> coursePlan.getWeek().getWeek()))
+                .filter((coursePlan1, coursePlan2) -> {
+                    Duration between = Duration.between(coursePlan1.getTimeSlot().getEndTime(), coursePlan2.getTimeSlot().getStartTime());
+                    return !between.isNegative() && between.compareTo(Duration.ofMinutes(30)) <= 0;
+                })
+                .reward(HardSoftScore.ONE_SOFT)
+                .asConstraint("Teacher time efficiency");
+    }
+
+
 }
